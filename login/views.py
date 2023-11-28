99224c048710edfa85baa8d5f95bd97fff002fbe
Dany248118 <ouen_daniel@hotmail.com>
2023-11-28 14:16:08 -0600

    Sustraccion de login

--- login/views.py
+++ login/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class Home(LoginRequiredMixin, generic.TemplateView):
+class Home( generic.TemplateView):
     template_name = 'index.html'
     login_url = 'login'
 
